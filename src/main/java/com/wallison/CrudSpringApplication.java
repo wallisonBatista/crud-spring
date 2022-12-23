e7d16a45bb043fb375438a72d594be0e62fc8d36
wallisonBatista <wallisongbatista@gmail.com>
2022-12-23 18:46:33 -0300

    Criado a tabela, conexão com o banco de dados h2, e alterando a forma como o id é enviado para o front-end

--- src/main/java/com/wallison/CrudSpringApplication.java
+++ src/main/java/com/wallison/CrudSpringApplication.java
@@ -1,13 +1,31 @@
 package com.wallison;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.wallison.model.Course;
+import com.wallison.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDataBase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+
+			Course c = new Course();
+			c.setName("angular e spring");
+			c.setCategory("front-end");
+
+			courseRepository.save(c);
+		};
 	}
 
 }
---
+++ src/main/java/com/wallison/controller/CourseController.java
@@ -0,0 +1,28 @@
+package com.wallison.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wallison.model.Course;
+import com.wallison.repository.CourseRepository;
+
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController {
+
+    private final CourseRepository courseRepository;
+
+    public CourseController(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    @GetMapping
+    public @ResponseBody List<Course> list() {
+        return courseRepository.findAll();
+    }
+
+}

53d51414c83c312205c7b2f395bde33894652158
wallisonBatista <wallisongbatista@gmail.com>
2023-01-08 16:56:03 -0300

    salvando o novo curso e alterando o código do status ao criar de cod  200 padrão da requisição para o status code correto ao criar 201

--- src/main/java/com/wallison/controller/CourseController.java
+++ src/main/java/com/wallison/controller/CourseController.java
@@ -2,9 +2,14 @@ package com.wallison.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wallison.model.Course;
@@ -23,6 +28,13 @@ public class CourseController {
     @GetMapping
     public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        // System.out.println(course.getName());
+        return courseRepository.save(course);
     }
 
 }

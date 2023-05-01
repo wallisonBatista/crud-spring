1c7c16e88aa7539c0fc36f5a48cbdbd383a7a5aa
Wallison Batista <51002039+wallisonBatista@users.noreply.github.com>
2023-05-01 11:16:28 -0300

    Soft Delete: remoção lógica e filtrando pelo status para exibir

--- src/main/java/com/wallison/model/Course.java
+++ src/main/java/com/wallison/model/Course.java
@@ -1,5 +1,7 @@
 package com.wallison.model;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -14,8 +16,11 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
+
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE Course SET status  = 'Inativo' WHERE id = ? ")
+@Where(clause = "status = 'Ativo' ")
 public class Course {
 
     @Id
@@ -35,4 +40,12 @@ public class Course {
     @Pattern(regexp = "Back-end|Front-end")
     @Column(length = 10, nullable = false)
     private String category;
+
+    @NotBlank
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Ativo|Inativo")
+    @Column(length = 10, nullable = false)
+    private String status = "Ativo";
+
 }
